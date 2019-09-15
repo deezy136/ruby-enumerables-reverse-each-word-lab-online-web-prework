5517991d22c470f85c2bc7844386c7e2d3e88e85
deezy13 <deezy13@gmail.com>
2019-09-15 18:10:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,14 @@
 
 def reverse_each_word(string)
 
-string_split = string.split
+string_split = string.split(" ")
+reverse_word = []
 
-string_split.size.times { | x| x}
+string_split.each { 
+  |stringS| reverse_word << stringS.reverse
+}
 
-string_split
+
 end
+
 reverse_each_word("Hello there, and how are you?")
